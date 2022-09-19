305df65d8d729402ff163549e4cc6edae990360a
mohith <mohith540@gmail.com>
2022-09-18 22:34:46 -0400

    catalouge

--- mongodb.sh
+++ mongodb.sh
@@ -1,35 +1,45 @@
 LOG_FILE=/tmp/mongodb
 
+statuscheck() {
+   if [ $? -eq 0 ]
+     then
+     echo status = SUCCESS
+     else
+     echo status = FAILURE
+     exit 1
+    fi
+ }
+
 echo "downloading repos"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
-echo status = $?
+statuscheck $?
 
 echo "installing nginx"
 yum install -y mongodb-org &>>$LOG_FILE
-echo status = $?
+statuscheck $?
 
 echo "changing ip address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-echo status = $?
+statuscheck $?
 
 echo "enabling mongodb"
 systemctl enable mongod &>>$LOG_FILE
-echo status = $?
+statuscheck $?
 
 echo "restarting mongodb"
 systemctl start mongod &>>$LOG_FILE
-echo status = $?
+statuscheck $?
 
 echo "downloading schema"
  curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG_FILE
-echo status =$?
+statuscheck $?
 
 
 cd /tmp
 
 echo "unzipping"
 unzip mongodb.zip &>>$LOG_FILE
-echo status = $?
+statuscheck $?
 
 
 cd mongodb-main
@@ -37,4 +47,4 @@ cd mongodb-main
  echo "loading service schema"
  mongo < catalogue.js &>>$LOG_FILE
  mongo < users.js &>>$LOG_FILE
-echo status = $?
+statuscheck $?
