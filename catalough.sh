06f24790c9a66f38d86e4c8f21586220189756d3
mohith <mohith540@gmail.com>
2022-09-18 21:22:10 -0400

    catalouge

---
+++ catalough.sh
@@ -0,0 +1,43 @@
+ LOG_FILE=/tmp/catalouge
+
+ echo "set repos"
+ curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
+ echo status = $?
+
+ echo"install nodejs"
+ yum install nodejs -y &>>$LOG_FILE
+echo status = $?
+
+echo "add user"
+ useradd roboshop &>>$LOG_FILE
+
+echo "download catalouge application code"
+ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG_FILE
+ echo status = $?
+
+ cd /home/roboshop
+
+ echo "unzipping"
+ unzip /tmp/catalogue.zip &>>$LOG_FILE
+ echo status = $?
+
+ mv catalogue-main catalogue &>>$LOG_FILE
+ cd /home/roboshop/catalogue &>>$LOG_FILE
+
+echo "installing npm"
+npm install &>>$LOG_FILE
+echo status = $?
+
+ mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+
+ echo "reloading"
+ systemctl daemon-reload &>>$LOG_FILE
+echo status = $?
+
+echo "starting"
+ systemctl start catalogue &>>$LOG_FILE
+ echo status = $?
+
+ echo "enabling"
+ systemctl enable catalogue &>>$LOG_FILE
+echo status = $?
