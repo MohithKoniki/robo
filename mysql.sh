326bb26a8b92878ae5db5068d2281eb8a671c8ae
mohith <mohith540@gmail.com>
2022-09-20 17:56:14 -0400

    mysql

---
+++ mysql.sh
@@ -0,0 +1,24 @@
+LOG_FILE=&>>/tmp/mysql
+
+soure common.sh
+
+
+echo "installing repo files"
+ curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>{LOG_FILE}
+ statuscheck $?
+
+ echo "disabling mysql files"
+ dnf module disable mysql  &>>{LOG_FILE}
+statuscheck $?
+
+echo "installing mysql files"
+ yum install mysql-community-server -y  &>>{LOG_FILE}
+statuscheck $?
+
+echo "start mysql"
+ systemctl enable mysqld  &>>{LOG_FILE}
+ systemctl start mysqld  &>>{LOG_FILE}
+ statuscheck $?
+
+ grep temp /var/log/mysqld.log
+
